c278d756e7dfb3483dadd47581d99297e70280a3
Arthur Bernardo <asbm@ic.ufal.br>
2020-02-19 00:58:56 -0300

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
@@ -15,14 +16,14 @@ int min_distance(int dist[], bool sptSet[])
     return min_index;
 }
 
-int print_solution(int dist[])
+int print_solution(int dist[], int src)
 {
-    cout << "Distance from source: " << endl;
+    cout << "Distance from: " << src << endl;
     for (int i = 0; i < V; i++)
         cout << i << " dist: " << dist[i] << endl;
 }
 
-void dijkstra(int graph[V][V], int src)
+void dijkstra(int graph[][3], int src)
 {
     int dist[V];
     bool sptSet[V];
@@ -41,21 +42,57 @@ void dijkstra(int graph[V][V], int src)
                 dist[v] = dist[u] + graph[u][v];
     }
 
-    print_solution(dist);
+    print_solution(dist, src);
 }
 
-int main()
+bool file_exist(string filename)
 {
-    int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
-                       {4, 0, 8, 0, 0, 0, 0, 11, 0},
-                       {0, 8, 0, 7, 0, 4, 0, 0, 2},
-                       {0, 0, 7, 0, 9, 14, 0, 0, 0},
-                       {0, 0, 0, 9, 0, 10, 0, 0, 0},
-                       {0, 0, 4, 14, 10, 0, 2, 0, 0},
-                       {0, 0, 0, 0, 0, 2, 0, 1, 6},
-                       {8, 11, 0, 0, 0, 0, 1, 0, 7},
-                       {0, 0, 2, 0, 0, 0, 6, 7, 0}};
+    ifstream f(filename);
+    return f.is_open();
+}
+
+bool get_file(int argc, char *argv[], ifstream &file)
+{
+    if (argc <= 1)
+    {
+        cout << "Incorrect arguments." << endl;
+        return false;
+    }
+    else if (!file_exist(argv[1]))
+    {
+        cout << "File " << argv[1] << " not found." << endl;
+        return false;
+    }
+
+    file.open(argv[1]);
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    ifstream file;
+
+    int src, weight, dest, v, e;
+
+    get_file(argc, argv, file);
+
+    file >> v;
+    file >> e;
+
+    int graph[v][3];
+
+    int count = 0;
+
+    while (file >> src && file >> dest && file >> weight)
+    {
+        graph[count][0] = src;
+        graph[count][1] = dest;
+        graph[count][2] = weight;
+
+        count++;
+    }
 
     dijkstra(graph, 0);
+
     return 0;
 }
